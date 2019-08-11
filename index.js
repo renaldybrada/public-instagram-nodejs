9976830dbb34b1afd5a0e63182f4e4391786cf51
Renaldy Brada <renaldybrada@gmail.com>
2019-08-11 09:37:23 +0700

    handling cors enable

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const dotenv = require('dotenv');
 const routersAPI = require('./routers/api');
 const app = express();
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //load environment variabel
 dotenv.config();
 
